38a84d53a89f39e07bdf629145bf65b461269e7a
Yasuyuki Nakanishi <yasu0519@MacBookPro.local>
2018-10-20 02:07:00 +0900

    async to sync

--- api.js
+++ api.js
@@ -10,54 +10,44 @@
 "use strict";
 
 
-function requestZabbixAPI(url, user, password, method, params, successHandler, errorHandler) {
+function requestZabbixAPIsync(url, method, params, auth) {  /* sync function */
+    let request = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": 1
+    };
+    if (auth) {
+        request["auth"] = auth;
+    } else {
+        request.id = 2;
+    }
+
+    var result = false;
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
+    xhr.open("POST", url, false); // sync
     xhr.setRequestHeader('Content-Type', "application/json-rpc");
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             if (xhr.status == 200) {
-                var x = new XMLHttpRequest();
-                x.open("POST", url, true);
-                x.setRequestHeader('Content-Type', "application/json-rpc");
-
-                x.onreadystatechange = function() {
-                    if (x.readyState == 4) {
-                        if (x.status == 200) {
-                            successHandler(JSON.parse(x.responseText));
-                        } else {
-                            errorHandler({ "error": x.status });
-                        }
-                        x = null;
-                    }
-                };
-
-                var respons = JSON.parse(xhr.responseText);
-                let request = {
-                    "jsonrpc": "2.0",
-                    "method": method,
-                    "params": params,
-                    "id": respons.id + 1,
-                    "auth": respons.result
-                };
-                x.send(JSON.stringify(request));
-
-            } else {
-                errorHandler({ "error": xhr.status });
+                result = JSON.parse(xhr.responseText).result;
             }
             xhr = null;
         }
     };
 
-    let request = {
-        "jsonrpc": "2.0",
-        "method": "user.login",
-        "params": {
-            "user": user,
-            "password": password
-        },
-        "id": 1
-    };
     xhr.send(JSON.stringify(request));
+    return result;
+}
+
+
+function requestZabbixUserLogin(url, user, password) {
+    return requestZabbixAPIsync(url, "user.login", { "user": user, "password": password }, false);
+}
+
+
+function requestZabbixAPI(url, user, password, method, params) {
+    let auth = requestZabbixUserLogin(url, user, password);
+    return requestZabbixAPIsync(url, method, params, auth);
 }
