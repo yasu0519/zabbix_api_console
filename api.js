5e913088a0089584737a43a8782ac4f9f5b43350
Yasuyuki Nakanishi <yasu0519@MacBookPro.local>
2018-12-09 21:20:21 +0900

    update

--- api.js
+++ api.js
@@ -3,51 +3,63 @@
 
 
     api.js
-
+    * request zabbix api *
 
     
  */
 "use strict";
 
 
-function requestZabbixAPIsync(url, method, params, auth) {  /* sync function */
-    let request = {
+/*
+
+    request ZabbixAPI
+
+*/
+function requestZabbixAPI(url, method, params, auth) {
+    //  make request body
+    var request = {
         "jsonrpc": "2.0",
         "method": method,
         "params": params,
-        "id": 1
+        "id": 1,
+        "auth": null
     };
     if (auth) {
-        request["auth"] = auth;
-    } else {
+        request.auth = auth;
         request.id = 2;
     }
-
-    var result = false;
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, false); // sync
-    xhr.setRequestHeader('Content-Type', "application/json-rpc");
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                result = JSON.parse(xhr.responseText).result;
+    //  Wrap XMLHttpRequest with 'Promise'
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.setRequestHeader('Content-Type', "application/json-rpc");
+        xhr.onload = function () {
+            if (request.status === 200) {
+                resolve(JSON.parse(xhr.responseText).result);
+            } else {
+                reject(Error('error code: ' + xhr.statusText));
             }
-            xhr = null;
-        }
-    };
-
-    xhr.send(JSON.stringify(request));
-    return result;
+        };
+        xhr.onerror = function () {
+           reject(Error('network error'));
+        };
+        xhr.send(JSON.stringify(request));
+    });
 }
 
+/*
 
-function requestZabbixUserLogin(url, user, password) {
-    return requestZabbixAPIsync(url, "user.login", { "user": user, "password": password }, false);
-}
+    get ZabbixAPI
 
-
-function requestZabbixAPI(url, user, password, method, params) {
-    let auth = requestZabbixUserLogin(url, user, password);
-    return requestZabbixAPIsync(url, method, params, auth);
+*/
+function getZabbixAPI(url, user, password, method, params) {
+    return new Promise(function (resolve, reject) {
+        requestZabbixAPI(url, "user.login", { "user": user, "password": password }, false)
+            .then(function (result) {
+                resolve(requestZabbixAPI(url, method, params, result));
+            })
+            .catch(function (error) {
+                reject(error);
+            });
+    });
 }
